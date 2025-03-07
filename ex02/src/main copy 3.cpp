cb75ee6681427a4e6012a0b4f3bbab99cd52c027
Tina <nguyen.valentina.99@gmail.com>
2025-03-07 18:08:04 +0100

    added

---
+++ ex02/src/main copy 3.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngtina1999 <ngtina1999@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/27 01:07:16 by ngtina1999        #+#    #+#             */
+/*   Updated: 2025/03/01 17:47:55 by ngtina1999       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <RPN.hpp>
+
+int		main(int argc, char **argv) {
+	
+	if (argc != 2) {
+		std::cerr << MYRED "Error: the program works with an expression as an argument" MYEOF << std::endl;
+		return (1);
+	}
+	try {
+		RPN rpn(argv[1]);
+		std::cout << rpn.getExpression() << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cerr << e.what() << std::endl;
+		return (1);
+	}
+
+}
+
