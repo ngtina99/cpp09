eba1d5ea3ea6417dc9c9d14a888a99722efa718a
Tina <nguyen.valentina.99@gmail.com>
2025-03-16 23:16:37 +0100

    added

--- ex02/inc/PmergeMe.hpp
+++ ex02/inc/PmergeMe.hpp
@@ -6,14 +6,30 @@
 /*   By: ngtina1999 <ngtina1999@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 19:29:54 by ngtina1999        #+#    #+#             */
-/*   Updated: 2025/03/09 19:32:10 by ngtina1999       ###   ########.fr       */
+/*   Updated: 2025/03/16 22:24:19 by ngtina1999       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#define PMERGE_ME_HPP
 #ifndef PMERGE_ME_HPP
+#define PMERGE_ME_HPP
 
-class 
+#include <exception>
+
+class PmergeMe {
+
+	public:
+
+		PmergeMe();
+		~PmergeMe();
+		PmergeMe& operator=(const PmergeMe& other);
+		PmergeMe(const PmergeMe& other);
+
+	private:
+
+		
+		
+
+}
 
 
 #endif
--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,12 +6,24 @@
 /*   By: ngtina1999 <ngtina1999@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 01:07:16 by ngtina1999        #+#    #+#             */
-/*   Updated: 2025/03/09 19:29:48 by ngtina1999       ###   ########.fr       */
+/*   Updated: 2025/03/16 22:14:51 by ngtina1999       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <PmergeMe.hpp>
 
-int	main() {
-	
+int	main(int argc, char **argv) {
+
+	for(int i = 0; i < argc; i++) {
+		argv[i];
+	}
+
+	try {
+
+	}
+
+	catch(const std::exception &e) {
+		
+	}
+
 }
