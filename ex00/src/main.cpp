f1ce4989c8ed7776fa4d59da6d93ae921d6f2a37
ngtina99 <nguyen.valentina.99@gmail.com>
2025-08-03 23:37:46 +0200

    ex00 format

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ngtina1999 <ngtina1999@student.42.fr>      +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 01:07:16 by ngtina1999        #+#    #+#             */
-/*   Updated: 2025/02/27 23:02:37 by ngtina1999       ###   ########.fr       */
+/*   Updated: 2025/08/03 23:36:56 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 
 int	main(int argc, char **argv)
 {
+
 	if (argc != 2) {
 		std::cerr << MYRED "Error: the program works with 1 filename as an argument" MYEOF << std::endl;
 		exit(1);
 	}
+
 	try {
 		BitcoinExchange bitcoinExchange;
 		bitcoinExchange.fileReader(argv[1]);
@@ -25,4 +27,5 @@ int	main(int argc, char **argv)
 	catch ( std::exception &e ) {
 		std::cerr << e.what() << std::endl;
 	}
+
 }
